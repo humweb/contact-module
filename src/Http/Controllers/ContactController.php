d7f031d3a35f39b250f371043412ecda7e3ca1c2
Ryan Shofner <ryan@letsgolearn.com>
2017-01-18 01:47:30 -0800

    load menu

--- src/Http/Controllers/ContactController.php
+++ src/Http/Controllers/ContactController.php
@@ -5,11 +5,17 @@ namespace Humweb\Contact\Http\Controllers;
 use Humweb\Contact\Facades\ContactMailer;
 use Humweb\Contact\Http\Requests\ContactFormRequest;
 use Humweb\Core\Http\Controllers\Controller;
-
+use Humweb\Menus\Models\MenuItem;
 
 class ContactController extends Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $menu = MenuItem::build_navigation(1);
+        $this->viewShare('menu', $menu);
+    }
     public function getForm()
     {
         return $this->setContent('contact::form');
